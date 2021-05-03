9391e244fb4da54ecbb6cd175c5e94f3f863218b
andy3_kuo <andy3_kuo@asus.com>
2021-05-03 13:17:51 +0800

    Add the script and Dockerfile to setup the yocto-3.2-imx_8m build environment
    Change-Id: I9c9277d2912eb2b3d48e1055132e836aa7cb70ce

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM ubuntu:20.04
+
+ARG DEBIAN_FRONTEND=noninteractive
+ARG userid
+ARG groupid
+ARG username
+
+# Install required packages for building Tinker Edge R Android
+# kmod: depmod is required by "make modules_install"
+RUN apt-get update && apt-get install -y gawk wget git diffstat unzip \
+    texinfo gcc-multilib build-essential chrpath socat cpio python python3 \
+    python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git \
+    python3-jinja2 libegl1-mesa libsdl1.2-dev pylint3 xterm rsync curl locales
+
+RUN locale-gen en_US.UTF-8
+
+ENV LANG en_US.UTF-8
+
+RUN groupadd -g $groupid $username && \
+    useradd -m -u $userid -g $groupid $username && \
+    echo "$username ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers && \
+    echo $username >/root/username
+
+ENV HOME=/home/$username
+ENV USER=$username
+WORKDIR /source
+
+ENTRYPOINT chroot --userspec=$(cat /root/username):$(cat /root/username) / /bin/bash -c "cd /source; /bin/bash -i"
+
---
+++ docker-builder-run.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -xe
+
+mkdir -p $HOME/yocto-3.2/share
+
+if [ -x "$(command -v docker)" ]; then
+    echo "Docker is installed and the execute permission is granted."
+    if getent group docker | grep &>/dev/null "\b$(id -un)\b"; then
+	echo "User $(id -un) is in the group docker."
+    else
+        echo "Docker is not managed as a non-root user."
+	echo "Please refer to the following URL to manage Docker as a non-root user."
+        echo "https://docs.docker.com/install/linux/linux-postinstall/"
+	exit
+    fi
+else
+    echo "Docker is not installed or the execute permission is not granted."
+    echo "Please refer to the following URL to install Docker."
+    echo "http://redmine.corpnet.asus/projects/configuration-management-service/wiki/Docker"
+    exit
+fi
+
+DIRECTORY_PATH_TO_DOCKER_BUILDER="$(dirname $(readlink -f $0))"
+echo "DIRECTORY_PATH_TO_DOCKER_BUILDER: $DIRECTORY_PATH_TO_DOCKER_BUILDER"
+
+DIRECTORY_PATH_TO_SOURCE="$(dirname $DIRECTORY_PATH_TO_DOCKER_BUILDER)"
+
+if [ $# -eq 0 ]; then
+    echo "There is no directory path to the source provided."
+    echo "Use the default directory path to the source [$DIRECTORY_PATH_TO_SOURCE]."
+else
+    DIRECTORY_PATH_TO_SOURCE=$1
+    if [ ! -d $DIRECTORY_PATH_TO_SOURCE ]; then
+        echo "The source directory [$DIRECTORY_PATH_TO_SOURCE] is not found."
+        exit
+    fi
+fi
+
+DOCKER_IMAGE="asus/yocto-3.2-builder:latest"
+docker build --build-arg userid=$(id -u) --build-arg groupid=$(id -g) --build-arg username=$(id -un) -t $DOCKER_IMAGE \
+    --file $DIRECTORY_PATH_TO_DOCKER_BUILDER/Dockerfile $DIRECTORY_PATH_TO_DOCKER_BUILDER
+
+OPTIONS="--interactive --privileged --rm --tty --network host"
+OPTIONS+=" --volume $DIRECTORY_PATH_TO_SOURCE:/source --volume $HOME/yocto-3.2/share:$HOME/yocto-3.2/share"
+OPTIONS+=" -v /usr/src:/usr/src -v /lib/modules:/lib/modules -v /linux-kernel:/linux-kernel"
+echo "Options to run docker: $OPTIONS"
+
+docker run $OPTIONS $DOCKER_IMAGE
